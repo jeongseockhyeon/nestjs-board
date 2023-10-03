09004d90d569752bb1f1f0ed662d235f48edccab
jsh <jeong9260@naver.com>
2023-10-03 21:39:08 +0900

    [ADD]게시물 삭제 전 존재 여부 확인

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -7,10 +7,11 @@ import { CreateBoardDto } from './dto/create-board.dto';
 export class BoardsService {
   private boards: Board[] = []; // private: 외부 클래스에서 접근하지 못하게 하는 명령어
 
+  //모든 게시물 조회
   getALlBoards(): Board[] {
     return this.boards;
   }
-
+  //게시물 생성
   createBoard(createBoardDto: CreateBoardDto) {
     const { title, description } = createBoardDto;
     const board: Board = {
@@ -22,6 +23,7 @@ export class BoardsService {
     this.boards.push(board);
     return board;
   }
+  // 특정 게시물 조회
   getBoardById(id: string): Board {
     const found = this.boards.find((board) => board.id === id);
     if (!found) {
@@ -29,9 +31,16 @@ export class BoardsService {
     }
     return found;
   }
+
+  //특정 게시물 삭제
   deleteBoard(id: string): void {
+    //특정 게시물 존재 여부 확인
+    const found = this.getBoardById(id);
+
     this.boards = this.boards.filter((board) => board.id !== id);
   }
+
+  //특정 게시물 상태 업데이트
   updateBoardStatus(id: string, status: BoardStatus): Board {
     const board = this.getBoardById(id);
     board.status = status;

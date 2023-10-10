b6a8d1602504880e4728e70e9587c567678301bc
jsh <jeong9260@naver.com>
2023-10-10 19:29:26 +0900

    [ADD] 인증된 유저만 게시글 보기

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,8 +3,9 @@ import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
+import { AuthModule } from 'src/auth/auth.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardRepository])],
+  imports: [TypeOrmModule.forFeature([BoardRepository]), AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService, BoardRepository],
 })

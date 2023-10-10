ec38042305bca9e738c713b7c4059c485229c2fa
jsh <jeong9260@naver.com>
2023-10-10 19:40:41 +0900

    [ADD]유저와 게시물 관계 현성

--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -1,5 +1,12 @@
-import { BaseEntity, PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+import {
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  ManyToOne,
+} from 'typeorm';
 import { BoardStatus } from './board-status.enum';
+import { User } from 'src/auth/user.entity';
 
 @Entity()
 export class Board extends BaseEntity {
@@ -14,4 +21,7 @@ export class Board extends BaseEntity {
 
   @Column()
   status: BoardStatus;
+
+  @ManyToOne((type) => User, (user) => user.boards, { eager: false })
+  user: User;
 }
